5e2e2bbe4d8a9ef2df097f06e21a3237432776b9
Iysah <yusufiysah@gmail>
2023-08-22 13:40:53 +0100

    developed our works page

---
+++ src/assets/portfolio/index.js
@@ -0,0 +1,21 @@
+import BoxA from './box-a.svg'
+import BoxB from './box-b.svg'
+import BoxC from './box-c.svg'
+
+import imgA from './imgA.svg'
+import imgB from './imgB.svg'
+import imageA from './imageA.svg'
+import imageB from './imageB.svg'
+
+import banner from './banner.svg'
+
+export {
+    BoxA,
+    BoxB,
+    BoxC,
+    imgA,
+    imgB,
+    banner,
+    imageA,
+    imageB,
+}
