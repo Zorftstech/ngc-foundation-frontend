6e55e04db804ea9a958ad76f906142d638ce2eaf
Iysah <yusufiysah@gmail>
2023-08-22 08:32:02 +0100

    developed FAQs page

--- src/app/faqs/page.js
+++ src/app/faqs/page.js
@@ -1,8 +1,53 @@
+import { Box, Container, Flex, Heading, Text } from '@chakra-ui/layout'
 import React from 'react'
 
+const qanda = [
+  {
+    question: 'Does NGC Foundation operate across Nigeria?',
+    answer: 'Yes we do!'
+  },
+  {
+    question: 'Can NGC Foundation receive international transfers?',
+    answer: 'Yes we do!'
+  },
+  {
+    question: 'Does the Ngo carry out tech related projects?',
+    answer: 'we absolutely do!'
+  },
+  {
+    question: 'Can i be a volunteer even when there is no opening?',
+    answer: 'yes we always accept volunteers, kindly send an email to us.'
+  },
+]
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Heading 
+      textAlign='center' 
+      fontSize='48px'
+      mt='3rem'
+      >FAQs</Heading>
+
+      <Flex justify={'center'} align='center' flexDir={'column'} my='2.5rem' py='2rem' px='10px' >
+        {qanda.slice(0, 5).map((item, index) => (
+          <Box key={index} 
+          w={{ base: '100%', lg: '840px'}} 
+          borderRadius='30px' 
+          bg='rgba(214, 10, 135, 0.04)' 
+          py={{ base: '20px', lg: '42px'}} px={{ base: '1rem', md: '2.5rem', lg: '6rem' }} my='1rem'>
+            <Flex justifyContent={'start'} alignItems='start' gap={'10px'}>
+              <Box bg='#3ADC30' w={'20px'} h='20px' borderRadius={'50%'} />
+  
+              <Box>
+                <Text fontWeight={'600'}>{item.question}</Text>
+                <Text fontWeight='400' color='#6D6D6D' >{item.answer}</Text>
+              </Box>
+            </Flex>
+          </Box>
+        ))}
+
+      </Flex>
+    </div>
   )
 }
 
