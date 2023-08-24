cd9116cde0133daf9f5f6a81d03d633547ef4b0f
Iysah <yusufiysah@gmail>
2023-08-24 06:58:05 +0100

    fixed merge conflicts

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,7 @@
 import Image from "next/image";
-<<<<<<< HEAD
 import Head from 'next/head';
 import HeroBanner from "@/components/HeroBanner";
 import AboutSection from "@/components/AboutSection";
-=======
->>>>>>> 1542ab850bfbcd9a4fbc17356ee1e704b4efe855
 import Beneficiaries from "@/components/Beneficiaries";
 import { AboutSection, Campaign, HeroBanner, Students } from "@/components";
 import Partners from "@/components/Partners";
@@ -12,20 +9,6 @@ import Sdgs from "@/components/Sdgs";
 
 export default function Home() {
 	return (
-<<<<<<< HEAD
-		<>
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>Ngozi Children Foundation</title>
-			</Head>
-			<main>
-				<HeroBanner />
-				<AboutSection />
-				<Beneficiaries />
-				<Students />
-			</main>
-		</>
-=======
 		<main>
 			<HeroBanner />
 			<AboutSection />
@@ -35,6 +18,5 @@ export default function Home() {
 			<Beneficiaries />
 			<Students />
 		</main>
->>>>>>> 1542ab850bfbcd9a4fbc17356ee1e704b4efe855
 	);
 }
